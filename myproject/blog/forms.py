8f42edcac152051c25143002d69fa9bfabba56bf
Elena <elena@Slash>
2016-01-26 16:49:31 +0300

    Blog project

--- myproject/blog/forms.py
+++ myproject/blog/forms.py
@@ -3,7 +3,6 @@ from django import forms
 from blog.models import Blog
 
 class CreatBlogForm(forms.ModelForm):
-    #title = forms.CharField(label= u'Заголовок',max_length=255)
-    #content = forms.CharField(label= u'Содержимое',widget=forms.Textarea(attrs={"cols":"40", "rows":"7"}))
+
     class Meta:
         model = Blog
--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
-
 class Blog(models.Model):
     title = models.CharField(u'Заголовок', max_length=255)
     content = models.TextField(u'Содержимое')
     creat_date = models.DateTimeField(u'Дата создания',auto_now_add=True)
     class Meta:
         db_table = "blog"
-#    def __unicode__(self):
-#        return self.title
